9094fe88ccbb9b641ef678b2b502b83649dc36d6
githubofgaurav <itsmegauravgupta@gmail.com>
2022-02-28 00:57:55 +0530

    adding data processing python code2

---
+++ data_processing.py
@@ -0,0 +1,12 @@
+from re import A
+
+
+print("adding all the data processing related code related to fraud detection")
+
+a = 5 
+b = 7
+def sum(a,b):
+    c = a + b
+    return a
+
+print ("data processing of code that is not correct")
