a9d487883bf6801ae29c595a9b380d0abb7297f8
githubofgaurav <itsmegauravgupta@gmail.com>
2022-02-28 00:48:46 +0530

    added a new function sum

---
+++ data_processing.py
@@ -0,0 +1,11 @@
+print("adding all the data processing code related to fraud detection")
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a+b
+    return a
+
+    
+print("added a neww function sum which is not correct")
